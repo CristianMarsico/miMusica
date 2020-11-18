58b2f6a944ef15cd68c73ccf87c83bc158b2ba6b
Cristian Marsico <cristianmarsico84@gmail.com>
2020-11-17 23:35:21 -0300

    implementacion two data binding

--- src/app/input-mas-menos/input-mas-menos.component.ts
+++ src/app/input-mas-menos/input-mas-menos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Cds } from '../lista-cds/Variables';
 
 
 @Component({
